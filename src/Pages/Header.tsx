856ecc370604181849b81ffef5f409f2f23022c6
Bradigson <bradigson@gmail.com>
2022-09-29 10:55:29 -0400

    Zona e Done

--- src/Pages/Header.tsx
+++ src/Pages/Header.tsx
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom";
 const Header = ()=>{
     return(
-        <nav className="navbar navbar-expand-lg bg-primary">
+        <nav className="navbar navbar-expand-lg bg-primary fixed-top">
             <div className="container-fluid">
-            <a className="navbar-brand text-light ps-4" href="#">PRM</a>
+            <a className="navbar-brand text-light ps-4" href="/">PRM</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
--- src/components/ZonaE/Zona_E_by_name.tsx
+++ src/components/ZonaE/Zona_E_by_name.tsx
@@ -14,6 +14,7 @@ interface Persona{
 
 
 export const ZonaEAllData = ()=>{
+    //state
     const [getByName, setGetByName] = useState<Persona[]>([]);
     const [getById, setGetById] = useState({
         id : "",
@@ -23,14 +24,30 @@ export const ZonaEAllData = ()=>{
         colegio_Electoral:"",
         coordinador:""
     });
+    const [getByCedula, setGetByCedula] = useState<Persona[]>([]);
+    const [getByTelefono, setGetByTelefono] = useState<Persona[]>([]);
+    const [getByColegioElectoral, setGetByColegioElectoral] = useState<Persona[]>([]);
+    const [getByCoordinador, setGetByCoordinador] = useState<Persona[]>([]);
+
+    //input value
     const [id, setId] = useState('');
     const [nombre, setNombre] = useState('');
+    const [cedula, setCedula] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [colegioElectoral, setColegioElectoral] = useState('');
+    const [coordinador, setCoordinador] = useState('');
 
+    //class
     const [hideGetByName, setHideByName] = useState('');
     const [hideGetById, setHideById] = useState('');
+    const [hideGetByCedula, setHideByCedula] = useState('');
+    const [hideGetByTelefono, setHideByTelefono] = useState('');
+    const [hideGetByColegioElectoral, setHideByColegioElectoral] = useState('');
+    const [hideGetByCoordinador, setHideByCoordinador] = useState('');
 
 
 
+    //by id
     const fetchZonaEById = async()=>{
           
         fetch(`https://www.bradigapi.somee.com/api/Zona_E/${id}`, {
@@ -47,14 +64,17 @@ export const ZonaEAllData = ()=>{
         .catch(err => console.log(err));
         
          setId('');
-         setHideByName('d-none');  
          setHideById('');
-
-    //     // fetch(`http://www.bradigapi.somee.com/api/Zona_E/Nombre/${name}`)
-    //     // .then(res=> res.json())
-    //     // .then(res=> console.log(res))
-    //     // .catch(error => console.log('error', error));
+         setHideByName('d-none');  
+         setHideByCedula('d-none');
+         setHideByTelefono('d-none');
+         setHideByColegioElectoral('d-none');
+         setHideByCoordinador('d-none');
+    
+         console.log(getById);
     }
+
+    //by nombre
     const fetchZonaE  = async ()=>{
         
         try{
@@ -63,8 +83,13 @@ export const ZonaEAllData = ()=>{
             setGetByName(data);
             setNombre('');
             setId('');
+
             setHideById('d-none');
             setHideByName('');  
+            setHideByCedula('d-none');
+            setHideByTelefono('d-none');
+            setHideByColegioElectoral('d-none');
+            setHideByCoordinador('d-none');
 
         }catch(err){
             console.log(err);
@@ -72,9 +97,85 @@ export const ZonaEAllData = ()=>{
     }
 
     
+    //by cedula
+    const getZonaEByCedula = async()=>{
+        cedula == '' ? console.log("Cedula file is empty") : (
+        fetch(`https://www.bradigapi.somee.com/api/Zona_E/Cedula/${cedula}`)
+        .then(res=> res.json())
+        .then(data=> setGetByCedula(data))
+        .catch(error => console.log('error', error))
+        );
+        
+        setCedula('');
+        setHideById('d-none');
+        setHideByName('d-none');  
+        setHideByCedula('');
+        setHideByTelefono('d-none');
+        setHideByColegioElectoral('d-none');
+        setHideByCoordinador('d-none');
+
+    }
+
+    //by telefono
+    const getZonaEByTelefono = async()=>{
+        telefono == '' ? console.log("Cedula file is empty") : (
+        fetch(`https://www.bradigapi.somee.com/api/Zona_E/Telefono/${telefono}`)
+        .then(res=> res.json())
+        .then(data=> setGetByTelefono(data))
+        .catch(error => console.log('error', error))
+        );
+
+        setTelefono('');
+        setHideById('d-none');
+        setHideByName('d-none');  
+        setHideByCedula('d-none');
+        setHideByTelefono('');
+        setHideByColegioElectoral('d-none');
+        setHideByCoordinador('d-none');
+
+    }
+
+    //by Colegio Electoral
+    const getZonaEByColegioElectoral = async()=>{
+        colegioElectoral == '' ? console.log("Cedula file is empty") : (
+        fetch(`https://www.bradigapi.somee.com/api/Zona_E/Colegio_Electoral/${colegioElectoral}`)
+        .then(res=> res.json())
+        .then(data=> setGetByColegioElectoral(data))
+        .catch(error => console.log('error', error))
+        );
+
+        setColegioElectoral('');
+        setHideById('d-none');
+        setHideByName('d-none');  
+        setHideByCedula('d-none');
+        setHideByTelefono('d-none');
+        setHideByColegioElectoral('');
+        setHideByCoordinador('d-none');
+
+    }
+
+
+
+    //by Coordinador
+    const getZonaEByCoordinador = async()=>{
+        coordinador == '' ? console.log("Cedula file is empty") : (
+        fetch(`https://www.bradigapi.somee.com/api/Zona_E/Cordinador/${coordinador}`)
+        .then(res=> res.json())
+        .then(data=> setGetByCoordinador(data))
+        .catch(error => console.log('error', error))
+        );
+        
+        setCoordinador('');
+        setHideById('d-none');
+        setHideByName('d-none');  
+        setHideByCedula('d-none');
+        setHideByTelefono('d-none');
+        setHideByColegioElectoral('d-none');
+        setHideByCoordinador('');
+    }
   
     return(
-        <div className='zona-e-all-data'>
+        <div className='zona-e-all-data pb-4'>
             <div className='mt-4 ps-3'>
                 <h2>Zona E</h2>
             </div>
@@ -82,98 +183,207 @@ export const ZonaEAllData = ()=>{
                 <div>
                     <div><strong>Id</strong></div>
                     <div>
-                    <input
-                        type='number'
-                        className='form-control'
-                        name='id'
-                        placeholder='Id...'
-                        value={id}
-                        onChange={(e)=> setId(e.target.value)}
+                        <input
+                            type='number'
+                            className='form-control'
+                            name='id'
+                            placeholder='Id...'
+                            value={id}
+                            onChange={(e)=> setId(e.target.value)}
                         />
                     </div>
                 </div>
                 <div>
                     <div><strong>Nombre</strong></div>
                     <div>
-                    <input
-                        type='text'
-                        className='form-control'
-                        name='nombre'
-                        placeholder='Nombre'
-                        value={nombre}
-                        onChange={(e)=>setNombre(e.target.value)}
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='nombre'
+                            placeholder='Nombre'
+                            value={nombre}
+                            onChange={(e)=>setNombre(e.target.value)}
                         />
                     </div>
                 </div>
                 <div>
                     <div><strong>Cedula</strong></div>
                     <div>
-                        <input type="text" className='form-control'/>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='cedula'
+                            placeholder='cedula'
+                            value={cedula}
+                            onChange={(e)=>setCedula(e.target.value)}
+                        />
                     </div>
                 </div>
                 <div>
                     <div><strong>Telefono</strong></div>
                     <div>
-                        <input type="text" className='form-control'/>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='telefono'
+                            placeholder='telefono'
+                            value={telefono}
+                            onChange={(e)=> setTelefono(e.target.value)}
+                        />
                     </div>
                 </div>
                 <div>
                     <div><strong>Colegio Electoral</strong></div>
                     <div>
-                        <input type="text" className='form-control'/>
+                        <input
+                            type='text'
+                            className='form-control'
+                            name='colegioElectoral'
+                            placeholder='Colegio Electoral'
+                            value={colegioElectoral}
+                            onChange={(e)=> setColegioElectoral(e.target.value)}
+                        />
                     </div>
                 </div>
                 <div>
                     <div><strong>Coordinador</strong></div>
                     <div>
-                        <input type="text" className='form-control'/>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        {
-                            id == '' ? <div></div>:<button onClick={fetchZonaEById}>Search</button>
-                        }
-                        {
-                            nombre == '' ? <div></div>:<button onClick={fetchZonaE}>Search</button>
-                        }
-                        
-                       
+                        <input
+                                type='text'
+                                className='form-control'
+                                name='coordinador'
+                                placeholder='Coordinador'
+                                value={coordinador}
+                                onChange={(e)=> setCoordinador(e.target.value)}
+                        />
                     </div>
                 </div>
                 
+                
+            </div>
+            <div className='text-center mt-4 p-2'>
+                    <div>
+                        {
+                            id == '' ? <div></div>:<button onClick={fetchZonaEById} className="btn-search shadow">Search</button>
+                        }
+                        {
+                            nombre == '' ? <div></div>:<button onClick={fetchZonaE} className="btn-search shadow">Search</button>
+                        }
+                        {
+                            cedula == '' ? <div></div>:<button onClick={getZonaEByCedula} className="btn-search shadow">Search</button>
+                        }
+                        {
+                            telefono == '' ? <div></div>:<button onClick={getZonaEByTelefono} className="btn-search shadow">Search</button>
+                        }
+                        {
+                            colegioElectoral == '' ? <div></div>:<button onClick={getZonaEByColegioElectoral} className="btn-search shadow">Search</button>
+                        }
+                        {
+                            coordinador == '' ? <div></div>:<button onClick={getZonaEByCoordinador} className="btn-search shadow">Search</button>
+                        }
+                    </div>
             </div>
 
-
             <div>
-            <div  className="container mt-5">
-                                <table className="table table-striped shadow">
-                                    <thead className='bg-primary text-light text-center'>
-                                        <tr>
-                                            <th scope="col">Id</th>
-                                            <th scope="col">Nombre</th>
-                                            <th scope="col">Cedula</th>
-                                            <th scope="col">Telefono</th>
-                                            <th scope="col">Colegio Electoral</th>
-                                            <th scope="col">Cordinador</th>
+                    <div  className="container mt-5">
+
+                        <table className="table table-striped table-hover shadow">
+                            <thead className='bg-primary text-light text-center'>
+                                <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Cedula</th>
+                                    <th scope="col">Telefono</th>
+                                    <th scope="col">Colegio Electoral</th>
+                                    <th scope="col">Cordinador</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                 {/* by id */}
+                            {
+                            getById != null ? (
+                                <tr className={`${hideGetById} text-center`}>
+                                    <td>{getById.id}</td>
+                                    <td>{getById.nombre}</td>
+                                    <td>{getById.cedula}</td>
+                                    <td>{getById.telefono}</td>
+                                    <td>{getById.colegio_Electoral}</td>
+                                    <td>{getById.coordinador}</td>
+                                </tr>
+                            ):("")
+                            }
+
+                            {/* by name */}
+                            {
+                            getByName.map(data=>{
+                                    return(
+                                        <tr key={data.id} className={`${hideGetByName} text-center`}>
+                                            <td>{data.id}</td>
+                                            <td>{data.nombre}</td>
+                                            <td>{data.cedula}</td>
+                                            <td>{data.telefono}</td>
+                                            <td>{data.colegio_Electoral}</td>
+                                            <td>{data.cordinador}</td>
                                         </tr>
-                                    </thead>
-                                   {
-                                    getById != null ? (
-                                        <tr className={`${hideGetById} text-center`}>
-                                            <td>{getById.id}</td>
-                                            <td>{getById.nombre}</td>
-                                            <td>{getById.cedula}</td>
-                                            <td>{getById.telefono}</td>
-                                            <td>{getById.colegio_Electoral}</td>
-                                            <td>{getById.coordinador}</td>
+                                    )
+                                })
+                            }
+
+                            {/* by cedula */}
+                            {
+                            getByCedula.map(data=>{
+                                return(
+                                    <tr key={data.id} className={`${hideGetByCedula} text-center`}>
+                                        <td>{data.id}</td>
+                                        <td>{data.nombre}</td>
+                                        <td>{data.cedula}</td>
+                                        <td>{data.telefono}</td>
+                                        <td>{data.colegio_Electoral}</td>
+                                        <td>{data.cordinador}</td>
+                                    </tr>
+                                )
+                            })
+                            }
+
+                            {/* by telefono */}
+                            {
+                                getByTelefono.map(data=>{
+                                    return(
+                                        <tr key={data.id} className={`${hideGetByTelefono} text-center`}>
+                                            <td>{data.id}</td>
+                                            <td>{data.nombre}</td>
+                                            <td>{data.cedula}</td>
+                                            <td>{data.telefono}</td>
+                                            <td>{data.colegio_Electoral}</td>
+                                            <td>{data.cordinador}</td>
                                         </tr>
-                                    ):("")
-                                   }
-                                   {
-                                    getByName.map(data=>{
+                                    )
+                                })
+                            }
+
+
+                            {/* by Colegio Electoral */}
+                            {
+                                getByColegioElectoral.map(data=>{
+                                    return(
+                                        <tr key={data.id} className={`${hideGetByColegioElectoral} text-center`}>
+                                            <td>{data.id}</td>
+                                            <td>{data.nombre}</td>
+                                            <td>{data.cedula}</td>
+                                            <td>{data.telefono}</td>
+                                            <td>{data.colegio_Electoral}</td>
+                                            <td>{data.cordinador}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+
+                                  {/* by Coordinador */}
+                                {
+                                    getByCoordinador.map(data=>{
                                         return(
-                                            <tr key={data.id} className={`${hideGetByName} text-center`}>
+                                            <tr key={data.id} className={`${hideGetByCoordinador} text-center`}>
                                                 <td>{data.id}</td>
                                                 <td>{data.nombre}</td>
                                                 <td>{data.cedula}</td>
@@ -183,13 +393,10 @@ export const ZonaEAllData = ()=>{
                                             </tr>
                                         )
                                     })
-                                   }
-                                   
-                                </table>
-
-                            </div>
-               
-
+                                }
+                            </tbody>
+                         </table>
+                    </div>
             </div>
         </div>
     )
